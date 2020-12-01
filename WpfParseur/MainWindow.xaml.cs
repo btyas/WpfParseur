3736704ec7b2350269b464fd020475d9668ecd81
btyas <terdayetyassine@gmail.com>
2020-12-01 11:41:35 +0100

    Ajoutez des fichiers projet.

---
+++ WpfParseur/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfParseur
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            String[] C_Romain = new string[] { "I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD","D","CM","M" };
+            String[] 
+        }
+
+        private void ConvertBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var RcText = this.EnterChiffreRomain.Text;
+
+            char[] CharRcText = RcText.ToCharArray();
+
+            for (int i =0;i<CharRcText.Length;i++)
+            {
+
+            }
+
+        }
+
+        private void EnterChiffreRomain_MouseEnter(object sender, MouseEventArgs e)
+        {
+
+        }
+    }
+}
