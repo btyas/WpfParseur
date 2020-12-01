b9b8061daab18ace16150ee4f67ea6d928beff55
btyas <terdayetyassine@gmail.com>
2020-12-01 12:17:59 +0100

    Valid1

--- WpfParseur/MainWindow.xaml.cs
+++ WpfParseur/MainWindow.xaml.cs
@@ -35,16 +35,109 @@ namespace WpfParseur
 
             char[] CharRcText = RcText.ToCharArray();
 
+            int resulatConversion = 0;
+            
             for (int i =0;i<CharRcText.Length;i++)
             {
+                int C2 = 0;
+                int C1 = 0;
+                if (CharRcText[i] == 'I')
+                {
+                    C1 = 1;
+                }
+                else if (CharRcText[i] == 'V')
+                {
+                    C1 = 5;
+                }
+                else if (CharRcText[i] == 'X')
+                {
+                    C1 = 10;
+                }
+                else if (CharRcText[i] == 'L')
+                {
+                    C1 = 50;
+                }
+                else if (CharRcText[i] == 'C')
+                {
+                    C1 = 100;
+                }
+                else if (CharRcText[i] == 'D')
+                {
+                    C1 = 500;
+                }
+                else if (CharRcText[i] == 'M')
+                {
+                    C1 = 1000;
+                }
+                else
+                {
+                    C1 = -1;
+                }
 
+                if (i + 1 < CharRcText.Length)
+                {
+                    // Getting value of symbol s[i+1]
+                    if (CharRcText[i+1] == 'I')
+                    {
+                        C2 = 1;
+                    }
+                    else if (CharRcText[i] == 'V')
+                    {
+                        C2 = 5;
+                    }
+                    else if (CharRcText[i] == 'X')
+                    {
+                        C2 = 10;
+                    }
+                    else if (CharRcText[i] == 'L')
+                    {
+                        C2 = 50;
+                    }
+                    else if (CharRcText[i] == 'C')
+                    {
+                        C2 = 100;
+                    }
+                    else if (CharRcText[i] == 'D')
+                    {
+                        C2 = 500;
+                    }
+                    else if (CharRcText[i] == 'M')
+                    {
+                        C2 = 1000;
+                    }
+                    else
+                    {
+                        C2 = -1;
+                    }
+
+                    // Comparing both values
+                    if (C1 >= C2)
+                    {
+
+                        resulatConversion = resulatConversion + C1;
+                    }
+                    else
+                    {
+
+                        resulatConversion = resulatConversion + C2 - C1;
+                        i++;
+                    }
+                }
+                else
+                {
+                    resulatConversion = resulatConversion + C1;
+                }
             }
 
-        }
+            ResultConvert.Text = resulatConversion.ToString();
 
-        private void EnterChiffreRomain_MouseEnter(object sender, MouseEventArgs e)
-        {
 
         }
-    }
+
+        }
+   
+      
+    
+    
 }
+
